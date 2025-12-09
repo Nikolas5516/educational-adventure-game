690ec18f67ea796bcc70b6dfb70327cda21cd7a9
Nikolas5516 <nikolas.gavaz@stud.ubbcluj.ro>
2025-12-09 19:15:48 +0200

    meniuprincipal.gd changed from 3 to 5

--- scenes/meniuprincipal.gd
+++ scenes/meniuprincipal.gd
@@ -12,7 +12,7 @@ const FINAL_CHEST_INDEX: int = 6
 
 # Variabila care stocheaza nivelul maxim deblocat de jucator (pentru test, incepem cu 1)
 # ATENTIE: In jocul final, aceasta variabila trebuie incarcata dintr-un sistem de salvare!
-var unlocked_level: int = 3 
+var unlocked_level: int = 5 
 
 
 # --- DIFICULTATE & SCENE NIVELURI ---
